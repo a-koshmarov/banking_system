fda3a1f0aebf68fbef9bfa0f5fe1d30d1581a4a2
Alexander Komshanov <a.komshanovjr@gmail.com>
2019-10-23 21:37:43 +0300

    Credit account support

--- banking.py
+++ banking.py
@@ -3,13 +3,15 @@ class BankingSystem:
         self.accounts = {
             "Account": [],
             "DebitAccount": [],
+            "CreditAccount": [],
         }
 
     def __repr__(self):
         acc_len = len(self.accounts["Account"])
         debit_len = len(self.accounts["DebitAccount"])
-        s = '''Banking system has {} regular accounts and {} debit accounts
-        '''.format(acc_len, debit_len)
+        credit_len = len(self.accounts["CreditAccount"])
+        s = '''Banking system has {} regular accounts, {} debit accounts
+        and {} credit accounts'''.format(acc_len, debit_len, credit_len)
         return s
 
     def add_account(self, account):
@@ -17,6 +19,8 @@ class BankingSystem:
             self.accounts["Account"].append(account)
         elif type(account) is DebitAccount:
             self.accounts["DebitAccount"].append(account)
+        elif type(account) is CreditAccount:
+            self.accounts["CreditAccount"].append(account)
         else:
             raise TypeError
 
@@ -64,6 +68,13 @@ class DebitAccount(Account):
             self.balance -= amount
         account.balance += amount
 
+class CreditAccount(Account):
+    def __init__(self, reg_date, name):
+        super(CreditAccount, self).__init__(reg_date, name)
+        self.debt = 0
+
+    def add_interest(self, percent):
+        self.debt *= 1 + percent
 
 def main():
     bank = BankingSystem()
